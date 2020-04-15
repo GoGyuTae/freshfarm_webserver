55089b67812c5361571ecf9e0e2c0aecc561711e
O <familye@naver.com>
2020-04-15 15:50:52 +0900

    OGE Commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var sequelize = require('sequelize');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/AG1/:id', function(req, res, next) {
+  models.freshtable.findOne(
+    {
+      where: {
+        machine_num : req.params.id
+      },
+      order: [
+        ['id', 'DESC']
+      ]
+    })
+    .then((freshtable) => {
+      res.json(
+        {
+        temperature: freshtable.temperature,
+        humidity: freshtable.humidity,
+        g_humidity: freshtable.g_humidity,});
+    });
+});
+
+router.get('/AG2', function(req, res, next) {
+  models.freshtable.findOne(
+    {
+      order: [
+        ['id', 'DESC'] // MN = 2
+      ]
+    })
+    .then((freshtable) => {
+      res.json(
+        {
+        temperature: freshtable.temperature,
+        humidity: freshtable.humidity,
+        g_humidity: freshtable.g_humidity,});
+    });
+});
+
+router.post('/AP1', function(req, res) {
+  const user_message = req.body.message;
+  res.status(200).json(
+    {
+      "message" : user_message
+      // 데이터 베이스에 데이터를 보냄
+    }
+  );
+});
+
+router.post('/AP2', function(req, res) {
+  const user_message = req.body.message;
+  res.status(200).json(
+    {
+      "message" : user_message
+    }
+  );
 });
 
 module.exports = router;
